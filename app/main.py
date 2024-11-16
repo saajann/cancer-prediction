3a15c39e5f0453c8846b6ca9bf2675735b7f98e7
saajan <sainisaaji@gmail.com>
2024-11-16 16:48:01 +0100

    created the machine learning model

---
+++ app/main.py
@@ -0,0 +1,5 @@
+def main():
+    print('creating a streamlit app')
+
+if __name__ == '__main__':
+    main()
--- model/main.py
+++ model/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+import pickle 
 
 def create_model(data):
     X = data.drop(['diagnosis'], axis=1)
@@ -17,10 +19,13 @@ def create_model(data):
     # training the model 
     model = LogisticRegression()
     model.fit(X_train, y_train)
-    
 
+    # test model
+    y_pred = model.predict(X_test)
+    print('Accuracy: ', accuracy_score(y_test, y_pred))
+    print('Classification Report: ', classification_report(y_test, y_pred))
 
-
+    return model, scaler    
 
 def get_clean_data():
     data = pd.read_csv("data/data.csv")
@@ -30,8 +35,14 @@ def get_clean_data():
 
 def main():
     data = get_clean_data()
-    print(data.head())
-    model = create_model(data)
+
+    model, scaler = create_model(data)
+
+    with open('model/model.pkl', 'wb') as f:
+        pickle.dump(model, f)
+        
+    with open('model/scaler.pkl', 'wb') as f:
+        pickle.dump(scaler, f)
 
 if __name__ == '__main__':
     main()
